31f28063c2a0ab6498adf0c612fde8193e8e9132
jaypac06 <jaydenpacker267@gmail.com>
2026-02-02 11:47:55 -0500

    worked on basic login page

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/feed")
 def feed():
     return render_template("feed.html")
 
@@ -17,6 +17,18 @@ def profile():
 @app.route("/createPost")
 def create_post():
     return render_template("create_post.html")
+
+@app.route("/", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
 
 if __name__ =="__main__":
     app.run(debug=True)
